aa6d3e1883cfbf4a12f649c029798a28fee451a2
nobmurakami <mhpdm541@gmail.com>
2021-04-04 08:30:15 +0900

    First Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// require_once('');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>クイズ</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <div class="content">
+    
+  </div>
+</body>
+</html>
